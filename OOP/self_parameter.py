5bf181d4b9d89eee0d1a2915c476e362cb1cd1c5
mdshiblinoman <mdshiblisharkar@gmail.com>
2026-05-05 08:15:29 +0600

    upload oop in python

---
+++ OOP/self_parameter.py
@@ -0,0 +1,73 @@
+'''
+The self Parameter
+The self parameter is a reference to the current instance of the class.
+It is used to access properties and methods that belong to the class.
+'''
+# Use self to access class properties:
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def greet(self):
+    print("Hello, my name is " + self.name)
+
+p1 = Person("Emil", 25)
+p1.greet()
+
+# Without self, Python would not know which object's properties you want to access:
+# The self parameter links the method to the specific object:
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def printname(self):
+    print(self.name)
+
+p1 = Person("Tobias")
+p2 = Person("Linus")
+
+p1.printname()
+p2.printname()
+
+# It does not have to be named self, you can call it whatever you like, but it has to be the first parameter of any method in the class:
+# Use the words myobject and abc instead of self:
+class Person:
+  def __init__(myobject, name, age):
+    myobject.name = name
+    myobject.age = age
+
+  def greet(abc):
+    print("Hello, my name is " + abc.name)
+
+p1 = Person("Emil", 36)
+p1.greet()
+
+# Access multiple properties using self:
+class Car:
+  def __init__(self, brand, model, year):
+    self.brand = brand
+    self.model = model
+    self.year = year
+
+  def display_info(self):
+    print(f"{self.year} {self.brand} {self.model}")
+
+car1 = Car("Toyota", "Corolla", 2020)
+car1.display_info()
+
+# Call one method from another method using self:
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+  def greet(self):
+    return "Hello, " + self.name
+
+  def welcome(self):
+    message = self.greet()
+    print(message + "! Welcome to our website.")
+
+p1 = Person("Tobias")
+p1.welcome()
+
